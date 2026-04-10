67dae554f28a35bfda86b8c7605382acee8129f9
quzaimiwork-gif <quzaimi.work@gmail.com>
2026-04-10 09:52:10 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,19 @@
 import telebot
 import os
-from google import genai
+import google.generativeai as genai
 
 BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 
 bot = telebot.TeleBot(BOT_TOKEN)
-client = genai.Client(api_key=GEMINI_API_KEY)
+
+genai.configure(api_key=GEMINI_API_KEY)
+
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 @bot.message_handler(func=lambda message: True)
 def reply(message):
-    response = client.models.generate_content(
-        model="gemini-1.5-flash",
-        contents=message.text
-    )
-
+    response = model.generate_content(message.text)
     bot.reply_to(message, response.text)
 
 print("Bot running...")
