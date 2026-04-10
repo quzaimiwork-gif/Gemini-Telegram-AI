124259a95ab78da2427e3e0dd5000abaf4a4b68c
quzaimiwork-gif <quzaimi.work@gmail.com>
2026-04-10 10:01:05 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,26 @@
 import telebot
 import os
-import google.generativeai as genai
+from google import genai
 
 BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
-genai.configure(api_key=GEMINI_API_KEY)
-
-model = genai.GenerativeModel("gemini-1.5-flash-latest")
+client = genai.Client(api_key=GEMINI_API_KEY)
 
 @bot.message_handler(func=lambda message: True)
 def reply(message):
     try:
-        response = model.generate_content(message.text)
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=message.text
+        )
+
         bot.reply_to(message, response.text)
+
     except Exception as e:
+        print("ERROR:", e)
         bot.reply_to(message, "Maaf, sistem tengah ada gangguan. Cuba lagi nanti.")
 
 print("Bot running...")
